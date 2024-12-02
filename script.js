53ea7d8f36c51f077a6836254499177dad7b34e0
Sharma <sharmasharmasrb@gmail.com>
2024-12-01 21:23:33 -0800

    add more logics

--- script.js
+++ script.js
@@ -11,8 +11,18 @@ closebtn.addEventListener("click", function () {
 });
 const submenu = document.querySelector(" .sub-menu");
 const list = document.querySelector(".sub-menu-list");
+const menuicon = document.querySelector(".sub-menu .sub-menu-icon");
 submenu.addEventListener("click", function(){
     list.classList.toggle("active");
+    menuicon.classList.toggle("rotate");
+
+});
+const submenu1 = document.querySelector(" .sub-menu1");
+const list1 = document.querySelector(".sub-menu-list1");
+const menuicon1 = document.querySelector(".sub-menu1 .sub-menu-icon1")
+submenu1.addEventListener("click", function(){
+    list1.classList.toggle("active");
+    menuicon1.classList.toggle("rotate");
 });
 // submenu.forEach((btn) => {
 //     btn.addEventListener("click", function (event) {
